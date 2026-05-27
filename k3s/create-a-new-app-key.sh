6e906255051b0f448a25befeefd3a459e72fddd0
Willians Patino <ppwj@yahoo.com>
2026-05-27 13:44:58 -0400

    Fix: hide secret data

--- k3s/create-a-new-app-key.sh
+++
@@ -1,9 +0,0 @@
-# No entras al pod. Generas desde tu terminal
-NEW_KEY=$(openssl rand -base64 32)
-
-echo '> Nueva APP KEY'
-echo $NEW_KEY
-
-echo '> APP KEY para insertar en el manifiesto'
-# Tu clave actual (texto plano)
-echo -n "$NEW_KEY" | base64
--- k3s/genera-APP-KEY.sh
+++ k3s/genera-APP-KEY.sh
@@ -5,4 +5,4 @@ echo "Tu nueva clave: $NEW_KEY"
 # Codificarla para el secret
 NEW_KEY_B64=$(echo -n "$NEW_KEY" | base64)
 
-kubectl patch secret dknotes-secrets -n dknotes --type='json' -p="[{'op': 'replace', 'path': '/data/app_key', 'value':'$NEW_KEY_B64'}]"
+## kubectl patch secret dknotes-secrets -n dknotes --type='json' -p="[{'op': 'replace', 'path': '/data/app_key', 'value':'$NEW_KEY_B64'}]"

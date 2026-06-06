ee7cd216ea02dd81d41726b3256df7b910f8826e
Willians Patino <ppwj@yahoo.com>
2026-06-06 12:46:11 -0400

    fix: fluxcd boostrap

--- fluxcd-bootstrap.sh
+++ fluxcd-bootstrap.sh
@@ -2,14 +2,14 @@ flux bootstrap github \
   --owner=datenmaniak \
   --repository=dk-notes \
   --branch=laravel-k3s \
-  --path=./k3s/kustomization
+  --path=./k3s/kustomization \
+  --components-extra=image-reflector-controller,image-automation-controller
 #\
 #  --force
-#
 
 # Forzar actualización de Flux
-flux reconcile source git flux-system
+#flux reconcile source git flux-system
 
 # Esperar activamente a que la kustomization se aplique con éxito
-echo "Esperando a que Flux aplique los cambios en el clúster..."
-flux reconcile kustomization flux-system --with-source
+#echo "Esperando a que Flux aplique los cambios en el clúster..."
+#flux reconcile kustomization flux-system --with-source

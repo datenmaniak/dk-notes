a8ae72775c9f42653cf99aa217f70827361f2a12
Willians Patino <ppwj@yahoo.com>
2026-06-13 12:24:43 -0400

    Fix: ImageUpdateAutomation message

---
+++ forzar-actualizacion-imagen-flux.sh
@@ -0,0 +1,3 @@
+#Una vez que Flux actualice la automatización dentro del clúster, dale un empujón al controlador de imágenes para que procese la nueva versión que subiste al registry de inmediato:
+
+flux reconcile image automation dknotes-laravel -n flux-system
--- run-after-git-push.sh
+++ run-after-git-push.sh
@@ -1,1 +1,3 @@
+#Fuerza a Flux a leer la corrección del manifiesto desde Git:
+
 flux reconcile source git flux-system

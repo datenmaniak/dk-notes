2e0f16bfe911ed9416ecee1d5cb791a40c6abcc2
Willians Patino <ppwj@yahoo.com>
2026-06-13 12:31:12 -0400

    Feat: show updated version by FluxCD

--- forzar-actualizacion-imagen-flux.sh
+++ forzar-actualizacion-imagen-flux.sh
@@ -1,3 +1,7 @@
-#Una vez que Flux actualice la automatización dentro del clúster, dale un empujón al controlador de imágenes para que procese la nueva versión que subiste al registry de inmediato:
+#Una vez que Flux actualice la automatización dentro
+#del clúster, dale un empujón al controlador de imágenes para que procese la nueva versión que subiste al registry de inmediato:
 
-flux reconcile image automation dknotes-laravel -n flux-system
+#flux reconcile image automation dknotes-laravel -n flux-system
+
+#Destraba el controlador de automatización de imágenes ejecutando el comando de reconciliación
+flux reconcile image update dknotes-laravel -n flux-system
